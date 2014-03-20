bb637eff076baf850c841424a7811cc52af93eac
badjr <badjr1@gmail.com>
2014-03-20 03:58:52 -0400

    Commit from Netbeans

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, world!");
-                
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello, world!");
 
+    }
 }
