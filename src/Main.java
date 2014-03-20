d39ca6743d6ceed324a1e0d45335ebdf4518fd19
badjr <badjr1@gmail.com>
2014-03-20 02:31:52 -0400

    From Eclipse

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, world!");
+		System.out.println("From Eclipse");
 	}
 
 }
